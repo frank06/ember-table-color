d1fdd11e0709445bb315a1e0a6513b557763883f
frank06 <me@frank06.net>
2016-06-03 05:20:37 +0600

    use jquery

--- app/components/item-table-row.js
+++ app/components/item-table-row.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'tr'
+  
+  tagName: 'tr',
+  
+  click() {
+    this.$().parent().find('tr').removeClass('colored');
+    this.$().addClass('colored');
+  }
+  
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
-  actions: {
     
-    assignColor(item) {
-      this.get('wrappedFilteredItems').forEach((wrapper) => {
-        if (wrapper.get('item') === item) {
-          wrapper.set('colored', 'colored');
-        } else {
-          wrapper.set('colored', null);
-        }
-      })
-      
-    }
-    
-  },
-  
   filteredItems: Ember.computed(() => {
     return [
       { title: "Item 1" },
@@ -24,15 +9,6 @@ export default Ember.Controller.extend({
       { title: "Item 3" },
       { title: "Item 4" }
     ]
-  }),
-  
-  wrappedFilteredItems: Ember.computed('filteredItems.@each', function() {
-    return this.get('filteredItems').map(item => {
-      return Ember.Object.create({ // wrapper object
-        item: item,
-        colored: null
-      });
-    });
   })
   
 });
